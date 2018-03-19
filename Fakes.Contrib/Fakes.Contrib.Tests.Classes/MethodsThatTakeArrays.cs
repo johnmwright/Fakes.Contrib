6d3fa743e29627c7f3d31a9817f94feaee84befa
John M. Wright <john@wrightfully.com>
2018-03-19 01:39:49 -0500

    adding support for shims with void returns.  Adding unit tests for a bunch of different types for regression testing.

---
+++ Fakes.Contrib/Fakes.Contrib.Tests.Classes/MethodsThatTakeArrays.cs
@@ -0,0 +1,34 @@
+﻿namespace Fakes.Contrib.Tests.Classes
+{
+    public interface IMethodsThatTakeArrays
+    {
+        void MethodTakesArray<T>(T[] inputs);
+        void MethodTakesParams<T>(params T[] inputs);
+    }
+
+    public class MethodsThatTakeArrays : IMethodsThatTakeArrays
+    {
+        public virtual void MethodTakesArray<T>(T[] inputs)
+        {
+        }
+
+        public virtual void MethodTakesParams<T>(params T[] inputs)
+        {
+        }
+
+    }
+
+    public class NonVirtualMethodsThatTakeArrays : IMethodsThatTakeArrays
+    {
+        public void MethodTakesArray<T>(T[] inputs)
+        {
+        }
+
+        public void MethodTakesParams<T>(params T[] inputs)
+        {
+        }
+
+    }
+
+
+}
